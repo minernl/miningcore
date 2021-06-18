e80f40534ae404c09d8e1c3a868ea8aa72e7e9fd
Kabilan VK <kabilan.vk@nortonlifelock.com>
2021-06-17 17:01:29 -0700

    Using env config to override default config

--- src/Miningcore/Program.cs
+++ src/Miningcore/Program.cs
@@ -2,15 +2,13 @@ using McMaster.Extensions.CommandLineUtils;
 using Miningcore.Configuration;
 using Miningcore.PoolCore;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace Miningcore
 {
     public class Program
     {
+        private const string EnvironmentConfig = "cfg";
         private static CommandOption dumpConfigOption;
         private static CommandOption shareRecoveryOption;
         private static ClusterConfig clusterConfig;
@@ -19,62 +17,71 @@ namespace Miningcore
         {
             PoolLogo.Logo();
 
-            var MiningCore = new CommandLineApplication(throwOnUnexpectedArg: false)
+            var miningCore = new CommandLineApplication(throwOnUnexpectedArg: false)
             {
                 Name = "dotnet Miningcore.dll",
                 FullName = "MiningCore 2.0 - Stratum Mining Pool Engine",
                 Description = "Stratum mining pool engine for Bitcoin and Altcoins",
-                ShortVersionGetter = () => $"- MinerNL build v{Assembly.GetEntryAssembly().GetName().Version.ToString(2)}",
-                LongVersionGetter = () => $"- MinerNL build v{Assembly.GetEntryAssembly().GetName().Version}",
+                ShortVersionGetter = () => $"- MinerNL build v{Assembly.GetEntryAssembly()?.GetName().Version.ToString(2)}",
+                LongVersionGetter = () => $"- MinerNL build v{Assembly.GetEntryAssembly()?.GetName().Version}",
                 ExtendedHelpText = "--------------------------------------------------------------------------------------------------------------"
             };
 
-            var versionOption = MiningCore.Option("-v|--version", "Version Information", CommandOptionType.NoValue);
-            var configFileOption = MiningCore.Option("-c|--config <configfile>", "Configuration File", CommandOptionType.SingleValue);
-            dumpConfigOption = MiningCore.Option("-dc|--dumpconfig", "Dump the configuration (useful for trouble-shooting typos in the config file)", CommandOptionType.NoValue);
-            shareRecoveryOption = MiningCore.Option("-rs", "Import lost shares using existing recovery file", CommandOptionType.SingleValue);
-            MiningCore.HelpOption("-? | -h | --help");
-            MiningCore.OnExecute( () =>
-            {
-                Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            var versionOption = miningCore.Option("-v|--version", "Version Information", CommandOptionType.NoValue);
+            var configFileOption = miningCore.Option("-c|--config <configfile>", "Configuration File", CommandOptionType.SingleValue);
+            dumpConfigOption = miningCore.Option("-dc|--dumpconfig", "Dump the configuration (useful for trouble-shooting typos in the config file)", CommandOptionType.NoValue);
+            shareRecoveryOption = miningCore.Option("-rs", "Import lost shares using existing recovery file", CommandOptionType.SingleValue);
+            miningCore.HelpOption("-? | -h | --help");
+            miningCore.OnExecute(() =>
+           {
+               Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
 
-                string configFile = "config_template.json";
+               var configFile = "config_template.json";
 
-                if(versionOption.HasValue())
-                {
-                    MiningCore.ShowVersion();
-                }
+               if(versionOption.HasValue())
+               {
+                   miningCore.ShowVersion();
+               }
 
-                // overwrite default config_template.json with -c | --config <configfile> file
-                if(configFileOption.HasValue())
-                {
-                    configFile = configFileOption.Value();
-                }
+               // overwrite default config_template.json with -c | --config <configfile> file
+               if(configFileOption.HasValue())
+               {
+                   configFile = configFileOption.Value();
+               }
 
-                // Dump Config to JSON output
-                if(dumpConfigOption.HasValue())
-                {
-                    clusterConfig = PoolCore.PoolConfig.GetConfigContent(configFile);
-                    PoolCore.PoolConfig.DumpParsedConfig(clusterConfig);
-                }
+               // Dump Config to JSON output
+               if(dumpConfigOption.HasValue())
+               {
+                   clusterConfig = PoolCore.PoolConfig.GetConfigContent(configFile);
+                   PoolCore.PoolConfig.DumpParsedConfig(clusterConfig);
+               }
 
-                // Shares recovery from file to database
-                if(shareRecoveryOption.HasValue())
-                {
-                    PoolCore.Pool.RecoverSharesAsync(shareRecoveryOption.Value()).Wait();
-                }
+               // Shares recovery from file to database
+               if(shareRecoveryOption.HasValue())
+               {
+                   Pool.RecoverSharesAsync(shareRecoveryOption.Value()).Wait();
+               }
 
-                if(!configFileOption.HasValue())
-                {
-                    MiningCore.ShowHelp();
-                }
-                else
-                {
-                    // Start Miningcore PoolCore
-                    PoolCore.Pool.StartMiningCorePool(configFile);
-                }
-            });
-            MiningCore.Execute(args);
+               if(configFileOption.HasValue())
+               {
+                   // Start Miningcore PoolCore
+                   Pool.StartMiningCorePool(configFile);
+               }
+               else
+               {
+                   var envConfig = Environment.GetEnvironmentVariable(EnvironmentConfig);
+                   if(!string.IsNullOrEmpty(envConfig))
+                   {
+                       // Start Miningcore PoolCore
+                       Pool.StartMiningCorePoolWithJson(envConfig);
+                   }
+                   else
+                   {
+                       miningCore.ShowHelp();
+                   }
+               }
+           });
+            miningCore.Execute(args);
         }
     }
 }
