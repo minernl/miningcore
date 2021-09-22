82bc2db1ab8755757ecb7d13f46a2ee73a377b07
Kabilan VK <kabilan.vk@nortonlifelock.com>
2021-09-22 19:52:42 -0400

    Merge updates

--- src/Miningcore/Blockchain/Ethereum/EthereumPool.cs
+++ src/Miningcore/Blockchain/Ethereum/EthereumPool.cs
@@ -204,11 +204,8 @@ namespace Miningcore.Blockchain.Ethereum
                 context.Stats.ValidShares++;
                 await UpdateVarDiffAsync(client);
 
-                TelemetryClient tc = TelemetryUtil.GetTelemetryClient();
-                if(null != tc)
-                {
-                    tc.GetMetric("ACCEPT_SHARES").TrackValue(difficulty);
-                }
+                var tc = TelemetryUtil.GetTelemetryClient();
+                tc?.GetMetric("ACCEPT_SHARES").TrackValue(difficulty);
             }
 
             catch(StratumException ex)
@@ -216,11 +213,8 @@ namespace Miningcore.Blockchain.Ethereum
                 // telemetry
                 PublishTelemetry(TelemetryCategory.Share, clock.UtcNow - tsRequest.Timestamp.UtcDateTime, false);
 
-                TelemetryClient tc = TelemetryUtil.GetTelemetryClient();
-                if(null != tc)
-                {
-                    tc.GetMetric("REJECT_SHARES", "Cause").TrackValue(difficulty, ex.Message);
-                }
+                var tc = TelemetryUtil.GetTelemetryClient();
+                tc?.GetMetric("REJECT_SHARES", "Cause").TrackValue(difficulty, ex.Message);
 
                 // update client stats
                 context.Stats.InvalidShares++;
