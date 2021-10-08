209daedd498b610e9b10579ce7ca98201f7af607
Kabilan VK <kabilan.vk@nortonlifelock.com>
2021-10-08 13:04:04 -0400

    Updated order of the steps and purge command

--- build.sh
+++ build.sh
@@ -1,34 +1,34 @@
 echo "**********************************************************************************"
-echo "Use `build.sh -all` to build C++ Libraries"
+echo "Use `build.sh all` to build C++ Libraries"
 echo "**********************************************************************************"
 
 echo "========================== STOP SERVICE ==========================================" && \
 /etc/init.d/poolservice stop && \
-sleep 3 && \
+sleep 2 && \
 echo "========================== PULL LATEST CODE ======================================" && \
 cd ~/miningcore && \
 git pull && \
 echo "========================== CLEAN OLD BUILD =======================================" && \
-rm -d -R -f build
-mkdir build
-echo "Done"
-
-if [ "$1" = "-all" ]; then 
-echo "========================== BUILD LIB =============================================" && \
-	cd ~/miningcore/src/Native/libmultihash && \
-	make clean && make && \
-	cp libmultihash.so ../../../build/
-	cd ~/miningcore/src/Native/libcryptonight && \
-	make clean && make && \
-	cp libcryptonight.so ../../../build/
-	cd ~/miningcore/src/Native/libcryptonote && \
-	make clean && make && \
-	cp libcryptonote.so ../../../build/
-fi
- 
+cd ~/miningcore/build && \
+find . -type f -not -name '*so' -delete && \
+echo "Done" && \
 echo "========================== BUILD SRC =============================================" && \
 cd ~/miningcore/src/ && \
 dotnet publish -c Release --framework net5.0  -o ../build  && \
+
+if [ "$1" = "all" ]; then
+echo "========================== BUILD LIB =============================================" && \
+cd ~/miningcore/src/Native/libmultihash && \
+make clean && make && \
+yes| cp -rf libmultihash.so ../../../build/ && \
+cd ~/miningcore/src/Native/libcryptonight && \
+make clean && make && \
+yes| cp -rf libcryptonight.so ../../../build/ && \
+cd ~/miningcore/src/Native/libcryptonote && \
+make clean && make && \
+yes| cp -rf libcryptonote.so ../../../build/
+fi
+
 echo "========================== START SERVICE =========================================" && \
 cd ~/miningcore/build && \
 /etc/init.d/poolservice start && \
