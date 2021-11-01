57b54b43b9b7cc271556bfeb7a45623248fbf90f
joel_michel <joel_michel@symantec.com>
2021-11-01 15:14:26 -0400

    sorting balances by amount so that we payout biggest balances first.

--- src/Miningcore/Persistence/Postgres/Repositories/BalanceRepository.cs
+++ src/Miningcore/Persistence/Postgres/Repositories/BalanceRepository.cs
@@ -95,7 +95,7 @@ namespace Miningcore.Persistence.Postgres.Repositories
             const string query = "SELECT b.poolid, b.address, b.amount, b.created, b.updated, MAX(p.created) AS paiddate FROM balances AS b " +
                                  "LEFT JOIN payments AS p ON  p.address = b.address AND p.poolid = b.poolid " +
                                  "WHERE b.poolid = @poolId AND b.amount >= @minimum " +
-                                 "GROUP BY b.poolid, b.address, b.amount, b.created, b.updated";
+                                 "GROUP BY b.poolid, b.address, b.amount, b.created, b.updated ORDER BY b.amount DESC";
 
             return (await con.QueryAsync<Entities.Balance>(query, new { poolId, minimum }))
                 .Select(mapper.Map<Balance>)
